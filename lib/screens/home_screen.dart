493bbe41f3eac4c653c1873d4f4b748fe245f008
Rakesh005 <srakeshswain005@gmail.com>
2022-06-20 12:17:19 +0530

    item list generate in homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import '../widgets/item_widget.dart';
+import '../models/catalog.dart';
 
 import '../widgets/drawer.dart';
 
 class HomeScreen extends StatelessWidget {
+  final dummyList = List.generate(6, (index) => CatalogModel.items[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Catalog App')),
-      body: const Center(child: Text('Welcome to flutter code')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
       drawer: MyDrawer(),
     );
   }
